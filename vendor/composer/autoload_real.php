42a1acfba3cf72925268bf96b60946815d50080a
Nils Hörrmann <nils.hoerrmann@hananils.de>
2025-05-26 14:44:11 +0200

    Composer updates + Rector

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitf57e8eaf1df27c6b28b354128032bbef
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitf57e8eaf1df27c6b28b354128032bbef', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitf57e8eaf1df27c6b28b354128032bbef', 'loadClassLoader'));
