4480e0219806631cd68eb67efcd6585cf9925497
Nils Hörrmann <nils.hoerrmann@hananils.de>
2026-03-12 15:55:06 +0100

    Clean-up

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,19 +1,19 @@
 <?php return array(
     'root' => array(
         'name' => 'hananils/kirby-blindtext',
-        'pretty_version' => 'dev-main',
-        'version' => 'dev-main',
-        'reference' => '6d878bf79531b2db8613205e0d883d5d35f62669',
+        'pretty_version' => '2.0.1',
+        'version' => '2.0.1.0',
+        'reference' => null,
         'type' => 'kirby-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'dev' => false,
+        'dev' => true,
     ),
     'versions' => array(
         'hananils/kirby-blindtext' => array(
-            'pretty_version' => 'dev-main',
-            'version' => 'dev-main',
-            'reference' => '6d878bf79531b2db8613205e0d883d5d35f62669',
+            'pretty_version' => '2.0.1',
+            'version' => '2.0.1.0',
+            'reference' => null,
             'type' => 'kirby-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
